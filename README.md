4eb25b91ec15af299b417745ddde91d19ec79c4d
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-01-16 10:13:46 -0600

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# nomiealexa
+# Nomie Pro Alexa Skill
 Track your life with Nomie Pro and the Amazon Alexa
+
+## Dependencies
+
+- An [AWS account](https://console.aws.amazon.com/console/home)
+- A ($9.99) [Nomie Pro API Key](https://connect.nomie.io)
+
+## Quick Start
+
+1. Create an [AWS Lambda](https://console.aws.amazon.com/console/home) function
+   using `alexa-nomie.py` as the code
+   - You can follow the [official Amazon
+     instructions](https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/developing-an-alexa-skill-as-a-lambda-function)
+     to give you a hand
+   - You can use `test_event.json` as your test template
+   - Consider extending the timeout beyond the default of 3 seconds (I raised mine to 10, which is likely excessive, but eliminated some sporadic errors e.g. [#1](https://github.com/n8henrie/alexa-wolfram-alpha/issues/1))
+   - Put your Nomie API Key into an environment variable called "NOMIE_KEY" and also put your Alexa Skill Identifier in an environement variable called "SKILL_ID". For help view the [documentation](http://docs.aws.amazon.com/lambda/latest/dg/env_variables.html)
+1. Create a new [Alexa
+   Skill](https://developer.amazon.com/edw/home.html#/skill/create) using
+   `intent_schema.json` and `sample_utterances.txt`
+1. **Don't publish** the skill (because it uses your personal Nomie API Key), leave it in development mode
+1. Test that it's working from the web interface during the creation of the
+   skill
+1. Test that it's working with your Echo
---
+++ alexa-nomie.py
@@ -0,0 +1,158 @@
+"""alexa-nomie.py
+Amazon Alexa skill Lambda code
+"""
+
+from __future__ import print_function
+import os
+import xml.etree.ElementTree as etree
+try:
+    from urllib.request import urlopen
+    from urllib.parse import urlencode
+except ImportError:
+    # Python2
+    from urllib2 import urlopen
+    from urllib import urlencode
+
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    if (event['session']['application']['applicationId'] !=
+            os.environ["SKILL_ID"]):
+        raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
+
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "wa_query":
+        return call_nomie(intent, session)
+    else:
+        raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+# --------------- Functions that control the skill's behavior ------------------
+
+
+def get_welcome_response():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = "Try saying track snack."
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = ("I didn't catch that. Tell me what to track.")
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def call_nomie(intent, session):
+    session_attributes = {}
+    should_end_session = True
+    reprompt_text = "I didn't catch that. Care to try again?"
+    speech_output = "Try telling me to track something you have setup in Nomie."
+
+    api_root = "https://api.nomie.io/v2/"
+
+    api_key = os.environ["NOMIE_KEY"]
+
+    item = intent['slots']['response'].get('value')
+    if query:
+
+        resp = urlopen(api_root + "push/" + api_key + '/action=track/label=' + item)
+
+        # TODO: Very request success
+
+        speech_output = "Succesfully tracked your " + item;
+
+    return build_response(session_attributes, build_speechlet_response(
+        intent['name'], speech_output, reprompt_text, should_end_session))
+
+# --------------- Helpers that build all of the responses ----------------------
+
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': 'SessionSpeechlet - ' + title,
+            'content': 'SessionSpeechlet - ' + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
